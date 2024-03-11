60b04d1124738800bdda3b193916000e655616b9
Psiko23 <tylerpittman2001@gmail.com>
2024-03-11 14:07:43 -0700

    layout/asset design

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './assets/css/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  return( 
+    <div>
+      <div className='hero-container'>
+        <div className='intro-container'>
+          <div className='introduction'>
+            <h1>Hello There, I'm</h1>
+            <h2>Tyler Pittman<span>,</span></h2>
+            <h3>and I build things for the web.</h3>
+          </div>
+          <div className='statement'>
+            <p>I am dedicated to crafting clean, intuitive designs that prioritize your users needs.</p>
+          </div>  
+        </div>
+        {/* <div className='space'></div> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          hello this is my first time lol
-        </p>
+      <div className='scroll-container'>
+        <h3>&darr;&nbsp; HERES SOME OF MY WORK &nbsp;&darr;</h3>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className='process'>
+        <div className='card'>
+          <img id="research" src="/src/assets/images/research.png" />
+        </div>
+        <div className='card'>
+          <img id="design" src="/src/assets/images/design.png" />
+          </div>
+        <div className='card'>
+          <img id="build" src="/src/assets/images/build.png" />
+        </div>
+        <div className='card'>
+          <img id="test" src="/src/assets/images/test.png" />
+        </div>
+        <div className='card'>
+          <img id="deploy" src="/src/assets/images/deploy.png" />
+        </div>
+      </div>
+    </div>
   )
 }
 
