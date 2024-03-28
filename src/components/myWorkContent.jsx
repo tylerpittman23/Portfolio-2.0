bcde31fcdff1d1b4fbd4b1918838c96a2a48d772
Psiko23 <tylerpittman2001@gmail.com>
2024-03-27 20:43:20 -0700

    added project

--- src/components/myWorkContent.jsx
+++ src/components/myWorkContent.jsx
@@ -3,7 +3,7 @@ import ProjectDetails from "./UI/projectDetails";
 import blackjackWildSrc from '../assets/images/blackjackWild.png';
 import notesAppSrc from '../assets/images/notesApp.png';
 import weatherAppSrc from '../assets/images/weatherApp.png';
-import scheduleManagerSrc from '../assets/images/scheduleManager.png';
+import googleBooksSrc from '../assets/images/googlebooks.png';
 
 function MyWorkContent() {
 
@@ -12,7 +12,7 @@ function MyWorkContent() {
             title: 'Blackjack Wild',
             description: 'Javscript based casino style BlackJack game!',
             src: blackjackWildSrc,
-            techs: ['HTML','CSS','JavaScript','Express','Node.js','MySQL','Handlebars.js','Sequelize'],
+            techs: ['HTML','CSS','JavaScript','Express','Node','MySQL','Handlebars','Sequelize'],
             gh: 'https://github.com/Justinh144/BlackJackWild',
             href: 'https://shielded-beyond-59466-3224172544c4.herokuapp.com/'
         },
@@ -20,7 +20,7 @@ function MyWorkContent() {
             title: 'Studdy Buddy',
             description: 'A note taking app built to use a file like structure to keep your notes organized',
             src: notesAppSrc,
-            techs: ['HTML','CSS','JavaScript','Express','Node.js','Handlebars.js','MySQL','Sequelize'],
+            techs: ['HTML','CSS','JavaScript','Express','Node','Handlebars','MySQL','Sequelize'],
             gh: 'https://github.com/tylerpittman23/Notes-App',
             href: ''
         },
@@ -33,12 +33,12 @@ function MyWorkContent() {
             href: 'https://tylerpittman23.github.io/Weather-App/'
         },
         {
-            title: 'Schedule Manager',
-            description: 'Keep your day organized and on track with the help of this app.',
-            src: scheduleManagerSrc,
-            techs: ['HTML','CSS','JavaScript','Day.js'],
-            gh: 'https://github.com/tylerpittman23/Work-Day-Scheduler',
-            href: 'https://tylerpittman23.github.io/Work-Day-Scheduler/'
+            title: 'Book Finder',
+            description: 'Find your next best read with this Google Books api search tool',
+            src: googleBooksSrc,
+            techs: ['HTML','CSS','JavaScript','Mongoose','Express','React','Apollo Client', 'Bootstrap' ],
+            gh: 'https://github.com/tylerpittman23/Book-Search-Engine',
+            href: 'https://book-search-engine-nfj7.onrender.com/'
         },
     ]
     
